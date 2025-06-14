56503da488f7d03bf94604e45ef5fc881bfde2f9
dynotics <93452388+dynotics@users.noreply.github.com>
2025-06-14 03:54:16 -0400

    Updated with models

---
+++ Assets/HairCycler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairCycler : MonoBehaviour
+{
+    public List<GameObject> hairStyles;           
+    public RectTransform scissorsRect;            
+    public RectTransform triggerZone;            
+
+    private int currentIndex = 0;
+    private bool canSwitch = true;
+
+    void Update()
+    {
+        if (RectOverlaps(scissorsRect, triggerZone) && canSwitch)
+        {
+            CycleHair();
+            StartCoroutine(SwitchCooldown());
+        }
+    }
+
+    void CycleHair()
+    {
+        hairStyles[currentIndex].SetActive(false);
+        currentIndex = (currentIndex + 1) % hairStyles.Count;
+        hairStyles[currentIndex].SetActive(true);
+    }
+
+    IEnumerator SwitchCooldown()
+    {
+        canSwitch = false;
+        yield return new WaitForSeconds(0.5f); // prevent rapid-fire switching
+        canSwitch = true;
+    }
+
+    bool RectOverlaps(RectTransform a, RectTransform b)
+    {
+        return RectTransformUtility.RectangleContainsScreenPoint(b, a.position, null);
+    }
+}
+
